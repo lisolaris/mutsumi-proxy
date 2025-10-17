c75c22a44d8cd358538fe5dc02c3da3e50719c55
lisolaris <ligeek@outlook.com>
2025-10-17 16:41:08 +0800

    Update: remove bing.css (unuseable in Firefox because security policy)

--- worker.js
+++ worker.js
@@ -8,6 +8,7 @@ export default {
     const url = new URL(request.url);
     const css_paths = ["/bing.css", "/bing-no-logo.css", "/bing-no-logo-search.css"]
 
+
     // / -_-
     if (url.pathname === "/")
       return new Response(`
@@ -24,6 +25,7 @@ export default {
     if (url.pathname == "/favicon.ico")
       return Response.redirect("https://s3.sorali.org/image/cucumber.ico", 302);
 
+
     // teapot
     if (url.pathname === "/teapot")
       return new Response("I'm a teapot", {
@@ -34,54 +36,8 @@ export default {
         }
       });
 
-    // /bing.css 用于获取 Bing 图片并替换 Firefox 新标签页的背景图片
-    if (css_paths.includes(url.pathname)) {
-      const bing = await fetch("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN");
-      const data = await bing.json();
-      const img_url = "https://www.bing.com" + data.images[0].url;
-      const copyright_text = data.images[0].copyright;
 
-      const logo_visibility_css = url.pathname.includes("no-logo") ? "visibility: hidden;" : "";
-      const search_visibility_css = url.pathname.includes("no-logo-search") ? "visibility: hidden;" : "";
-      const css = `
-      @-moz-document url("about:newtab") {
-        .search-inner-wrapper {
-          ${search_visibility_css}
-        }
-        .logo-and-wordmark-wrapper {
-          ${logo_visibility_css}
-        }
-        .activity-stream {
-          background-image: url("${img_url}") !important;
-          background-size: cover !important;
-          background-attachment: fixed !important;
-        }
-        .activity-stream::before {
-          content: "${copyright_text.replace(/"/g, '\\"')}";
-          position: absolute;
-          bottom: 20px;
-          right: 20px;
-          left: unset;
-          font-size: 14px;
-          color: gray;
-          text-shadow: 0 0 4px rgba(0,0,0,0.6);
-          z-index: 9999;
-
-          background-color: rgba(0, 0, 0, 0.2);
-          padding: 8px 12px;
-          border-radius: 8px;
-          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15);
-          user-select: text;
-        }
-      }`;
-
-      return new Response(css, {
-        headers: { "content-type": "text/css; charset=utf-8" }
-      });
-    }
-
-
-  //  /api/cors/ 代理请求所给出的地址并为响应结果添加CORS跨域访问
+  //  /cors/ 代理请求所给出的地址并为响应结果添加CORS跨域访问
   if (url.pathname.startsWith("/api/cors/")) {
       const targetUrlString = url.toString().replace(url.origin + "/api/cors/", "");
 
@@ -119,7 +75,8 @@ export default {
       return newResp;
   }
 
-    // default docker hub proxy
+
+  // default docker hub proxy
     return dockerProxyHandler(request);
   }
 };
